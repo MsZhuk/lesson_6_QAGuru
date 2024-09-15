319caf80ff46874b07acf97e1ca9feb57fdfc290
Olya <ozhuk.fw@gmail.com>
2024-09-15 23:41:24 +0300

    update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,5 @@ export * from './main.page';
 export * from './register.page';
 export * from './settings.page';
 export * from './app.page';
+export * from './url.page';
+export * from './article.page'
