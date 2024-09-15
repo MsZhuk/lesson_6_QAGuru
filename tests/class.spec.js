941e9dcaa1bd4eafceffffb20e4635831f3ced3e
Olya <ozhuk.fw@gmail.com>
2024-09-15 23:42:40 +0300

    update class.spec.js

--- tests/class.spec.js
+++ tests/class.spec.js
@@ -1,6 +1,8 @@
 import { test, expect } from '@playwright/test';
 import { fa, faker } from '@faker-js/faker';
-import { MainPage, RegisterPage, SettingsPage} from "../src/pages/index";
+import { ArticlePage, MainPage, RegisterPage, SettingsPage} from "../src/pages/index";
+import { UrlPage } from '../src/pages/url.page';
+import { ADDRGETNETWORKPARAMS } from 'dns';
 
 
 const url = 'https://realworld.qa.guru/#/';
@@ -38,23 +40,18 @@ test('Пользователь может изменить bio', async ({ page }
 
 test('Добавить первую статью', async ({ page }) => {
   const mainPage = new MainPage(page);
-  const settingsPage = new SettingsPage(page);
-
+  const articlePage = new ArticlePage(page);
   await mainPage.clickArticle();
-  await page.getByPlaceholder('Article Title').click();
-  await page.getByPlaceholder('Article Title').fill('My first article');
-  await page.getByPlaceholder("What's this article about?").click();
-  await page.getByPlaceholder("What's this article about?").fill('About something');
-  await page.getByPlaceholder('Write your article (in markdown)').click();
-  await page.getByPlaceholder('Write your article (in markdown)').fill(newUser.textArticle);
-  await page.getByPlaceholder('Enter tags').click();
-  await page.getByPlaceholder('Enter tags').fill('mytag');
-  await page.getByRole('button', 'Update Publish Article').click();
+  await articlePage.writeArticleTitle();
+  await articlePage.writeArticleAbout();
+  await articlePage.writeArticleText(newUser.textArticle);
+  await articlePage.writeArticleTag();
+  await articlePage.sendArticle();
 });
 
 test('Обновить имя пользователя', async ({ page }) => {
   const mainPage = new MainPage(page);
- 
+
   await mainPage.goToSettings();
   await page.getByPlaceholder('Your Name').click();
   await page.getByPlaceholder('Your Name').clear();
@@ -64,10 +61,9 @@ test('Обновить имя пользователя', async ({ page }) => {
 
 test('Добавить url', async ({ page }) => {
   const mainPage = new MainPage(page);
- 
+  const urlPage = new UrlPage(page);
   await mainPage.goToSettings();
-  await page.getByPlaceholder('URL of profile picture').click();
-  await page.getByPlaceholder('URL of profile picture').fill(newUser.urlPicture);
+  await urlPage.enterUrl(newUser.urlPicture);
   await mainPage.updateSettings();  
 });
 
@@ -75,7 +71,7 @@ test('Переход на стартовую страницу', async ({ page })
   await page.locator('.navbar-brand').click();
 });
 
-test('проверка logout', async ({ page }) => {
+test('Проверка logout', async ({ page }) => {
   const mainPage = new MainPage(page);
   await mainPage.goToSettings();
   await mainPage.logout();
