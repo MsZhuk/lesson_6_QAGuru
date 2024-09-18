b4f72aa964d003e4555a98877d40a9ef07eaf2fb
Olya <ozhuk.fw@gmail.com>
2024-09-18 23:03:17 +0300

    update settings

--- src/pages/settings.page.js
+++ src/pages/settings.page.js
@@ -5,26 +5,34 @@ constructor (page) {
     super(page);
     this.updateButton = page.getByRole('button','Update Settings');
     this.bioField = this.page.getByPlaceholder('Short bio about you');
-}
+    this.urlString = this.page.getByPlaceholder('URL of profile picture');
+};
 
 async enterUserBio (bio) {
     await this.bioField.click();
     await this.bioField.fill(bio);
-}
+};
 
-async updateProfileSimple () {
+async updateProfileButton() {
     await this.updateButton.click();
-}
+};
+
 async getProfile () {
     const user = {
         bio: await this.bioField
     }
   return user;
-}
+};
 
 async updateProfile (bio = '') {
     await this.bioField.click();
     await this.bioField.fill(bio);
     await this.updateButton.click();
-}
+};
+
+async enterUrl(urlPicture){
+    await this.urlString.click();
+    await this.urlString.fill(urlPicture);
+  };
+
 }
